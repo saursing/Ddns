97a80d3f400618411f82e42c9ac4fd3982a83d21
saursing <33507188+saursing@users.noreply.github.com>
2023-02-21 23:08:20 -0800

    added more comments

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+# Import necessary modules
 import os
 import re
 import ipaddress
+import json
 from datetime import datetime
 from google.cloud import dns
 from googleapiclient.discovery import build
@@ -8,51 +10,46 @@ from flask import Flask, request, make_response, jsonify
 from oauth2client.client import GoogleCredentials
 from googleapiclient import discovery
 
-#Initialize Flask app
-app = Flask(name)
-
-#Set project ID and managed zone name
+# Initialize Flask app and set project ID and managed zone
+app = Flask(__name__)
 PROJECT_ID="rb-munish-playground"
 MANAGED_ZONE="my-zone-testing"
 
-#Define function to check if IPv4 address is valid
+# Function to check if an IP address is valid
 def is_valid_ipv4(ip_address):
-try:
-ipaddress.IPv4Address(ip_address)
-return True
-except ValueError:
-return False
+    try:
+        ipaddress.IPv4Address(ip_address)
+        return True
+    except ValueError:
+        return False
 
-#Define function to check if URL is valid
+# Function to check if a URL is in the correct format
 def is_valid_url(URL):
-# Check if the URL is in the format "test.domain.com."
-match_object = re.match("[a-zA-Z]+.[a-zA-Z]+.[a-zA-Z]+.", URL)
-if match_object:
-return True
-else:
-return False
+    # Check if the URL is in the format "test.domain.com."
+    match_object = re.match("[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+\.", URL)
+    if match_object:
+        return True
+    else:
+        return False
 
-#Define function to process requests
+# Function to process the HTTP request
 def process_request(request, operation):
-    # Extract URL and IP address from request
-    match_object = re.match("URL=(.)&IP=(.)", request)
+    # Extract the URL and IP address from the request string
+    match_object = re.match("URL=(.*)&IP=(.*)", request)
     URL = match_object.group(1)
     ip_address = match_object.group(2)
-
-    # Check if IP address is valid
+    # Check if the IP address is valid
     if not is_valid_ipv4(ip_address):
         return jsonify({"error": "Invalid IP address"}), 400
-
-    # Check if URL is valid
+    # Check if the URL is in the correct format
     if not is_valid_url(URL):
         return "Error: The URL {} is not in the correct format (example.com.)".format(URL)
-
-    # Initialize DNS client and credentials
+    # Create a DNS client and get the Google credentials
     client = dns.Client(project=PROJECT_ID)
     credentials = GoogleCredentials.get_application_default()
+    # Build the DNS API service
     service = discovery.build('dns', 'v1', credentials=credentials)
-
-    # Create DNS record set data
+    # Create a DNS record set data object with the URL and IP address
     record_set_data = {
         "kind": "dns#resourceRecordSet",
         "name": URL,
@@ -60,9 +57,8 @@ def process_request(request, operation):
         "ttl": 50,
         "type": "A",
     }
-
     try:
-        # Perform specified operation (add or delete) on DNS record
+        # If the operation is "add", create a new DNS record
         if operation == "add":
             service.changes().create(
                 project=PROJECT_ID,
@@ -71,6 +67,7 @@ def process_request(request, operation):
                     "additions": [record_set_data]
                 }
             ).execute()
+        # If the operation is "delete", delete the DNS record
         elif operation == "delete":
             service.changes().create(
                 project=PROJECT_ID,
@@ -88,19 +85,22 @@ def process_request(request, operation):
                     "message": error_message
                 }
             }
-            return (json.dumps(error_body), 400, {'Content-Type': 'application/json'})
+            return (json.dumps(error_body), 400, {'Content-Type': 'application/json'})            
     except Exception as e:
         # Return a 500 Internal Server Error code with an error message if an exception occurs
         return jsonify({"error": str(e)}), 500
-    # Return a success message
+    # Return a success message if the operation was successful
     return jsonify({"message": "DNS record {} successfully".format(operation)}), 200
 
 #Define endpoint for adding DNS record
 @app.route("/addIP/<request>")
 def add_dns(request):
-return process_request(request, "add")
+    return process_request(request, "add")
 
 #Define endpoint for deleting DNS record
 @app.route("/deleteIP/<request>")
 def delete_dns(request):
-return process_request
+    return process_request(request, "delete")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
